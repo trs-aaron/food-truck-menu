be0bad790297b770413f910dbd48261232c3f814
Aaron Arterburn <aarterburn@tamarackidaho.com>
2020-07-29 17:03:47 -0600

    Added menu item scrolling.

--- script/main.js
+++ script/main.js
@@ -3,15 +3,49 @@
     const FOOD_ITEMS_TMPL_ID = 'tam-menu-food-items-TEMPLATE';
     const BEV_ITEMS_TMPL_ID = 'tam-menu-bev-items-TEMPLATE';
 
-
-    class MenuItem {
+    class MenuItemField {
 
         constructor(el) {
             this._el = el;
         }
 
+        get top() {
+            return this._el.getBoundingClientRect().top;
+        }
+
+        get bottom() {
+            return this._el.getBoundingClientRect().bottom;
+        }
+
+        init() {
+            return;
+        }
+    }
+
+    MenuItemField.EL_CLASS = 'tam-menu_item_field'
+
+
+    class MenuItem {
+
+        constructor(el) {
+            this._el = el;
+            this._fields = [];
+        }
+
+        get isAvailable() {
+            return (this._el.getAttribute(MenuItem.AVAILABLE_ATTR) === '1');
+        }
+
         get isVisible() {
-            this._el.getAttribute(MenuItem.VISIBLE_ATTR);
+            return (this._el.getAttribute(MenuItem.VISIBLE_ATTR) === '1');
+        }
+
+        get top() {
+            return (this._fields.length > 0) ? Math.min(...this._fields.map((f) => f.top)) : 0;
+        }
+
+        get bottom() {
+            return (this._fields.length > 0) ? Math.max(...this._fields.map((f) => f.bottom)) : 0;
         }
 
         hide() {
@@ -23,11 +57,19 @@
         }
 
         init() {
-            return;
+            this._fields = [];
+            let fieldEls = this._el.getElementsByClassName(MenuItemField.EL_CLASS);
+
+            Array.from(fieldEls).forEach((el) => {
+                let field = new MenuItemField(el);
+                field.init();
+                this._fields.push(field);
+            });
         }
     }
 
     MenuItem.EL_CLASS = 'tam-menu_item';
+    MenuItem.AVAILABLE_ATTR = 'data-is-available';
     MenuItem.VISIBLE_ATTR = 'data-is-visible';
 
 
@@ -35,10 +77,38 @@
 
         constructor(el) {
             this._el = el;
+            this._itemsEl = null;
             this._items = [];
         }
 
+        get top() {
+            return this._el.getBoundingClientRect().top;
+        }
+
+        get bottom() {
+            return this._el.getBoundingClientRect().bottom;
+        }
+
+        get itemsTop() {
+            return this._itemsEl.getBoundingClientRect().top;
+        }
+
+        get itemsBottom() {
+            return this._itemsEl.getBoundingClientRect().bottom;
+        }
+
+        areAllItemsVisible() {
+            for (let i = 0; i < this._items.length; i++) {
+                if (!this._items[i].isVisible) {
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         init() {
+            this._itemsEl = this._el.getElementsByClassName(MenuItemCntr.ITEMS_EL_CLASS)[0];
             this._items = [];
             let itemEls = this._el.getElementsByClassName(MenuItem.EL_CLASS);
 
@@ -58,6 +128,10 @@
         }
 
         async progressItems() {
+            if (this.areAllItemsVisible()) {
+                return;
+            }
+
             let lastVisIndex = this._getLastVisibleIndex();
             let firstNewVisIndex = 0;
 
@@ -72,12 +146,55 @@
             await this._showItems(firstNewVisIndex);
         }
 
+        _getFirstVisibleIndex() {
+            for (let i = 0; i < this._items.length; i++) {
+                if ( this._items[i].isVisible) {
+                    return i;
+                }
+            }
+
+            return null;
+        }
+
         _getLastVisibleIndex() {
+            let firstVisIndex = this._getFirstVisibleIndex();
+
+            if (firstVisIndex !== null && !isNaN(firstVisIndex)) {
+                for(let i = firstVisIndex; i < this._items.length; i++) {
+                    if (!this._items[i].isVisible) {
+                        return (i - 1);
+                    }
+                }
+            }
+
             return null;
         }
 
         _isItemInFocus(index) {
-            return true;
+            let itemTop = this._items[index].top;
+            let itemBot = this._items[index].bottom;
+            let cntrTop = this.top;
+            let cntrBot = this.bottom;
+
+            return (this._items[index] !== null) ? (this._items[index].top >= this.top && this._items[index].bottom <= this.bottom) : false;
+        }
+
+        async _setItemsTransform(y) {
+            return new Promise((resolve, reject) => {
+                if (isNaN(y)) {
+                    reject();
+                }
+
+                const newTransform = `translateY(${y}px)`;
+
+                window.requestAnimationFrame(() => {
+                    this._itemsEl.style.transform = newTransform;
+                    this._itemsEl.style.MozTransform = newTransform;
+                    this._itemsEl.style.webkitTransform = newTransform;
+                    this._itemsEl.style.msTransform = newTransform;
+                    resolve();
+                });
+            });
         }
 
         async _hideItems(delay=250) {
@@ -125,18 +242,21 @@
         }
 
         async _moveToItem(index) {
-            return new Promise((resolve) => {
+            return new Promise(async (resolve, reject) => {
                 // Parameter check.
                 if (index === null || isNaN(index)) {
                     reject();
                 }
 
-                resolve();
+                let iTop = this._items[index].top;
+                let isTop = this.itemsTop;
+                let newTop = (this._items[index]) ? (this._items[index].top - this.itemsTop) : 0;
+                this._setItemsTransform(-newTop).then(resolve).catch(reject);
             });
         }
     }
 
-    MenuItemCntr.ITEM_CNTR_EL_CLASS = 'tam-menu_item-cntr';
+    MenuItemCntr.ITEMS_EL_CLASS = 'tam-menu_item-cntr';
 
 
     class Menu {
@@ -222,8 +342,8 @@
 
 
     async function render(config) {
-        let foodCntrEl = document.getElementById(Menu.FOOD_CNTR_ID).getElementsByClassName(MenuItemCntr.ITEM_CNTR_EL_CLASS)[0];
-        let bevCntrEl = document.getElementById(Menu.BEV_CNTR_ID).getElementsByClassName(MenuItemCntr.ITEM_CNTR_EL_CLASS)[0];
+        let foodCntrEl = document.getElementById(Menu.FOOD_CNTR_ID).getElementsByClassName(MenuItemCntr.ITEMS_EL_CLASS)[0];
+        let bevCntrEl = document.getElementById(Menu.BEV_CNTR_ID).getElementsByClassName(MenuItemCntr.ITEMS_EL_CLASS)[0];
         let foodItemsTmpl = document.getElementById(FOOD_ITEMS_TMPL_ID).innerHTML;
         let bevItemsTmpl = document.getElementById(BEV_ITEMS_TMPL_ID).innerHTML;
 
